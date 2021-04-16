8aceabbecbaed6525f739162dac4cf691c1c0441
David Li <davidli012345@gmail.com>
2021-04-16 04:16:48 +0000

    fixing logging issues

--- ai-trading-system/src/application/clients/logger_client.py
+++ ai-trading-system/src/application/clients/logger_client.py
@@ -2,24 +2,6 @@ import logging
 from application.utils.discord_logger import Discord_Handler
 from config.config import DISCORD_WEBHOOK
 
-DEBUG_LEVEL_BUY_NUM = 10
-DEBUG_LEVEL_SELL_NUM = 11
-DEBUG_LEVEL_ALERT_NUM = 12
-def buy(self, message, *args, **kws):
-    if self.isEnabledFor(DEBUG_LEVEL_BUY_NUM):
-        # Yes, logger takes its '*args' as 'args'.
-        self._log(DEBUG_LEVELV_NUM, message, args, **kws) 
-
-def sell(self, message, *args, **kws):
-    if self.isEnabledFor(DEBUG_LEVEL_SELL_NUM):
-        # Yes, logger takes its '*args' as 'args'.
-        self._log(DEBUG_LEVELV_NUM, message, args, **kws) 
-
-def alert(self, message, *args, **kws):
-    if self.isEnabledFor(DEBUG_LEVEL_ALERT_NUM):
-        # Yes, logger takes its '*args' as 'args'.
-        self._log(DEBUG_LEVELV_NUM, message, args, **kws) 
-
 class LoggerClient:
 
     _config = None
@@ -27,12 +9,9 @@ class LoggerClient:
     def __init__(self, config):
         self._config = config.LOG_CONFIG
         # TODO add custom logging
-        logging.addLevelName(DEBUG_LEVEL_BUY_NUM, "BUY")
-        logging.Logger.buy = buy
-        logging.addLevelName(DEBUG_LEVEL_SELL_NUM, "SELL")
-        logger.Logger.sell = sell
-        logging.addLevelName(DEBUG_LEVEL_ALERT_NUM, "ALERT")
-        logger.Logger.alert = alert
+        self.addLoggingLevel("buy", 10)
+        self.addLoggingLevel("sell", 11)
+        self.addLoggingLevel("alert", 12)
 
     def get_logger(self):
         logger = logging.getLogger(self._config['name'])
@@ -44,3 +23,52 @@ class LoggerClient:
         discord_handler = Discord_Handler(DISCORD_WEBHOOK)
         logger.addHandler(discord_handler)
         return logger
+
+    def addLoggingLevel(levelName, levelNum, methodName=None):
+        """
+        Comprehensively adds a new logging level to the `logging` module and the
+        currently configured logging class.
+
+        `levelName` becomes an attribute of the `logging` module with the value
+        `levelNum`. `methodName` becomes a convenience method for both `logging`
+        itself and the class returned by `logging.getLoggerClass()` (usually just
+        `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+        used.
+
+        To avoid accidental clobberings of existing attributes, this method will
+        raise an `AttributeError` if the level name is already an attribute of the
+        `logging` module or if the method name is already present 
+
+        Example
+        -------
+        >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+        >>> logging.getLogger(__name__).setLevel("TRACE")
+        >>> logging.getLogger(__name__).trace('that worked')
+        >>> logging.trace('so did this')
+        >>> logging.TRACE
+        5
+
+        """
+        if not methodName:
+            methodName = levelName.lower()
+
+        if hasattr(logging, levelName):
+            raise AttributeError('{} already defined in logging module'.format(levelName))
+        if hasattr(logging, methodName):
+            raise AttributeError('{} already defined in logging module'.format(methodName))
+        if hasattr(logging.getLoggerClass(), methodName):
+            raise AttributeError('{} already defined in logger class'.format(methodName))
+
+        # This method was inspired by the answers to Stack Overflow post
+        # http://stackoverflow.com/q/2183233/2988730, especially
+        # http://stackoverflow.com/a/13638084/2988730
+        def logForLevel(self, message, *args, **kwargs):
+            if self.isEnabledFor(levelNum):
+                self._log(levelNum, message, args, **kwargs)
+        def logToRoot(message, *args, **kwargs):
+            logging.log(levelNum, message, *args, **kwargs)
+
+        logging.addLevelName(levelNum, levelName)
+        setattr(logging, levelName, levelNum)
+        setattr(logging.getLoggerClass(), methodName, logForLevel)
+        setattr(logging, methodName, logToRoot)
