70e3d6041863a10aedfc2bc60928ae617f40946e
David Li <davidli012345@gmail.com>
2021-10-15 14:28:00 +0000

    updating check_ta

--- src/tests/ta/check_ta.py
+++ src/tests/ta/check_ta.py
@@ -3,9 +3,9 @@ sys.path.insert(0,'..')
 from application.clients.logger_client import LoggerClient
 from application.clients.ta_client import TAClient
 from config import config
+df = None
 logger = LoggerClient(config)
 taClient = TAClient(logger,config)
-
 df = taClient.stock_ta_for_cat("aapl", "momentum")
 df.to_csv('read.csv')
 df_interest = df[["RSI_14", "RSX_14", "RVGI_14_4", "ROC_10"]]
