e4d6f046d68f0ce10d72f3a5f59f4379ed2649d1
David Li <davidli012345@gmail.com>
2022-01-01 16:35:53 -0800

    feat: updating version and server logic

--- ai_trading_bot/server.py
+++ ai_trading_bot/server.py
@@ -1,8 +1,8 @@
 import logging
 from aiohttp import web
-
+from ai_trading_bot.application import __version__
 async def hello(request):
-    return web.Response(text="Hello, world")
+    return web.Response(text=f"Hello, world __version__: {__version__}")
 
 app = web.Application()
 app.router.add_get('/', hello)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 setup(
     name='Grandfleet trading',
-    version='1.0.0',
+    version='1.0.1',
     description='Grandfleet\'s trading bot',
     packages=find_packages(exclude=['*tests']),
     include_package_data=True,
